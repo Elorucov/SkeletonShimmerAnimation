49f6442b507dd9b71edbb8a8b39342d973137fd0
Elorucov <elorucov@live.ru>
2022-04-16 21:35:15 +0400

    Using opacity animation for 1703.

--- SkeletonShimmerAnimation/SkeletonAnimation.cs
+++ SkeletonShimmerAnimation/SkeletonAnimation.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Numerics;
 using Windows.Foundation;
+using Windows.Foundation.Metadata;
 using Windows.UI;
 using Windows.UI.Composition;
 using Windows.UI.Xaml;
@@ -51,6 +52,8 @@ namespace SkeletonShimmerAnimation {
             if (container == null) throw new ArgumentNullException(nameof(container));
             if (animatableElements == null && animatableElements.Count == 0) throw new ArgumentException("Value does not null or empty!", nameof(animatableElements));
 
+            bool isGradientSupported = ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 5);
+
             Color transparent = Color.FromArgb(0, color.R, color.G, color.B);
             await System.Threading.Tasks.Task.Delay(10); // needed to getting correct values from TransformPoint.
 
@@ -73,40 +76,53 @@ namespace SkeletonShimmerAnimation {
                 System.Diagnostics.Debug.WriteLine($"DS: {Math.Round(-diffs, 2)}; DE: {Math.Round(diffe, 2)}");
 
                 Compositor _compositor = ElementCompositionPreview.GetElementVisual(container).Compositor;
-                SpriteVisual _maskVisual;
+                SpriteVisual _maskVisual = _compositor.CreateSpriteVisual();
                 CompositionSurfaceBrush _surfBrush = (CompositionSurfaceBrush)shape.GetAlphaMask();
                 CompositionMaskBrush _maskBrush = _compositor.CreateMaskBrush();
                 _maskBrush.Mask = _surfBrush;
 
-                var gradient = _compositor.CreateLinearGradientBrush();
-                gradient.ColorStops.Add(_compositor.CreateColorGradientStop(0f, transparent));
-                gradient.ColorStops.Add(_compositor.CreateColorGradientStop(0.2f, color));
-                gradient.ColorStops.Add(_compositor.CreateColorGradientStop(0.4f, color));
-                gradient.ColorStops.Add(_compositor.CreateColorGradientStop(0.5f, transparent));
-
-                gradient.StartPoint = new Vector2(-diffs, 0f);
-                gradient.EndPoint = new Vector2(diffe, 0f);
-                gradient.Offset = new Vector2(-(float)container.ActualWidth / 2);
-                _maskBrush.Source = gradient;
-
-                _maskVisual = _compositor.CreateSpriteVisual();
                 _maskVisual.Brush = _maskBrush;
                 _maskVisual.Size = new Vector2((float)shape.ActualWidth, (float)shape.ActualHeight);
-
                 ElementCompositionPreview.SetElementChildVisual(shape, _maskVisual);
-
                 var animation = _compositor.CreateScalarKeyFrameAnimation();
-                animation.InsertKeyFrame(1, (float)container.ActualWidth * 2, _compositor.CreateLinearEasingFunction());
-                animation.Duration = TimeSpan.FromSeconds(1);
-                animation.IterationBehavior = AnimationIterationBehavior.Forever;
-                gradient.StartAnimation("Offset.X", animation);
 
+                if (isGradientSupported) {
+                    var gradient = _compositor.CreateLinearGradientBrush();
+                    gradient.ColorStops.Add(_compositor.CreateColorGradientStop(0f, transparent));
+                    gradient.ColorStops.Add(_compositor.CreateColorGradientStop(0.25f, color));
+                    gradient.ColorStops.Add(_compositor.CreateColorGradientStop(0.35f, color));
+                    gradient.ColorStops.Add(_compositor.CreateColorGradientStop(0.5f, transparent));
+
+                    gradient.StartPoint = new Vector2(-diffs, 0f);
+                    gradient.EndPoint = new Vector2(diffe, 0f);
+                    gradient.Offset = new Vector2(-(float)container.ActualWidth / 2);
+                    _maskBrush.Source = gradient;
+
+                    animation.InsertKeyFrame(1, (float)container.ActualWidth * 2, _compositor.CreateLinearEasingFunction());
+                    animation.Duration = TimeSpan.FromSeconds(1);
+                    animation.IterationBehavior = AnimationIterationBehavior.Forever;
+                    gradient.StartAnimation("Offset.X", animation);
+
+                    shape.Unloaded += (a, b) => {
+                        gradient.Dispose();
+                    };
+                } else {
+                    var colorb = _compositor.CreateColorBrush();
+                    colorb.Color = color;
+                    _maskBrush.Source = colorb;
+
+                    animation.InsertKeyFrame(0f, 0, _compositor.CreateLinearEasingFunction());
+                    animation.InsertKeyFrame(0.5f, 1, _compositor.CreateLinearEasingFunction());
+                    animation.InsertKeyFrame(1f, 0, _compositor.CreateLinearEasingFunction());
+                    animation.Duration = TimeSpan.FromSeconds(1);
+                    animation.IterationBehavior = AnimationIterationBehavior.Forever;
+                    _maskVisual.StartAnimation("Opacity", animation);
+                }
                 shape.Unloaded += (a, b) => {
                     _compositor.Dispose();
                     _surfBrush.Dispose();
                     _maskVisual.Dispose();
                     _maskBrush.Dispose();
-                    gradient.Dispose();
                     animation.Dispose();
                 };
             }
